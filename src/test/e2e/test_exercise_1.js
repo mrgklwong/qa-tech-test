9ecbcf30d097ba96032dad0e74526b6faf662967
Gary Ka Lee Wong <gary.ka.lee.wong@gmail.com>
2019-02-09 17:24:20 +0000

    Spent a 5 hours wondering why webdriverIo wasnt working :pen checking it in to figure out

--- src/test/e2e/test_exercise_1.js
+++ src/test/e2e/test_exercise_1.js
@@ -1,17 +1,29 @@
-const { remote } = require('webdriverio');
+var casper = require('casper').create();
+// casper.options.waitTimeout = 10000; //setting timeout to 10secs.
 
-(async () => {
-    const browser = await remote({
-        logLevel: 'error',
-        path: '/',
-        capabilities: {
-            browserName: 'firefox'
-        }
-    });
+casper.options.viewportSize = { width: 950, height: 950 };
 
-    await browser.url('http://localhost:3000');
+casper.start('http://localhost:3000/', function() {
+    this.echo("Loaded Page with page title => "+this.getTitle());
+    casper.capture('screenshots/1.png');
 
-    const title = await browser.getTitle();
+});
 
-    await browser.deleteSession();
-})().catch((e) => console.error(e));
+casper.waitForSelector("button[data-test-id=\"render-challenge\"]", function () {
+    this.click('button[data-test-id="render-challenge"]');
+    this.echo('Pressed button with selector  => button[data-test-id="render-challenge"]');
+    casper.capture('screenshots/2.png');
+});
+
+
+// casper.waitForSelector("td[data-test-id=\"array-item-1-0\"]", function () {
+//     var text = casper.evaluate(function(){
+//         return document.querySelector("d[data-test-id=\"array-item-1-0\"]").textContent;
+//     });
+//     this.echo("first row and column loaded with this number  => "+text);
+//     var firstRowArray = [];
+//     firstRowArray[0] = text;
+//     this.echo("first row array is  => "+ firstRowArray[0]);
+//
+// });
+
